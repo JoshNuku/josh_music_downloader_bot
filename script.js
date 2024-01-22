996615d4a21ca6c958b83b98f80a8e9738b39f78
Josh <nukujosh119@gmail.com>
2024-01-22 10:00:13 +0000

    Set environment variables

--- script.js
+++ script.js
@@ -1,14 +1,15 @@
 const { google } = require("googleapis");
+const { chat } = require("googleapis/build/src/apis/chat");
+require("dotenv").config();
 const TelegramBot = require("node-telegram-bot-api");
-const bot = new TelegramBot("6887856184:AAFKdStAjli3liT6LSL2rO-QZZtzYJPBt2Y", {
+const bot = new TelegramBot(process.env.TELEGRAM_BOT_TOKEN, {
   polling: true,
 });
-let videoLink, imageUrl, videoTitle;
 
 // Create a new YouTube client
 const youtube = google.youtube({
   version: "v3",
-  auth: "AIzaSyA1q6O9KIrtlBsdK7l8AR5OkalMdB_phKA", // Replace with your actual API key
+  auth: process.env.YOUTUBE_API_KEY, // Replace with your actual API key
 });
 
 // Set the API parameters
@@ -17,45 +18,47 @@ bot.onText(/\/start/, (msg) => {
   const chatId = msg.chat.id;
   bot.sendMessage(
     chatId,
-    `Hello! Welcome to Music downloader bot.
-     Search any song name and click the link to download`
+    `Hello! Welcome to Josh's music downloader bot.
+Search any song by name and artiste and click the link to download`
   );
 });
 bot.on("message", async (msg) => {
   const chatId = msg.chat.id;
-  // Define the search query
   const searchQuery = msg.text;
-  const params = {
-    part: "snippet",
-    q: searchQuery,
-    type: "video",
-  };
+  console.log(msg);
+  if (searchQuery[0] !== "/") {
+    // Define the search query
+    const params = {
+      part: "snippet",
+      q: searchQuery,
+      type: "video",
+    };
 
-  // Make the API request
-  youtube.search.list(params, async (err, response) => {
-    if (err) {
-      console.error("Error making API request:", err);
-      return;
-    }
+    // Make the API request
+    youtube.search.list(params, async (err, response) => {
+      if (err) {
+        console.error("Error making API request:", err);
+        return;
+      }
 
-    // Process the response
-    const videos = await response.data.items;
-    videos.forEach((video) => {
-      videoTitle = video.snippet.title;
-      const videoId = video.id.videoId;
-      videoLink = `https://www.ssyoutube.com/watch?v=${videoId}`;
-      imageUrl = video.snippet.thumbnails.default.url;
+      // Process the response
+      const videos = await response.data.items;
+      videos.forEach(async (video) => {
+        const videoTitle = await video.snippet.title;
+        const videoId = await video.id.videoId;
+        const videoLink = `https://www.azyoutube.com/watch?v=${videoId}`;
+        const imageUrl = await video.snippet.thumbnails.default.url;
 
-      console.log("Title:", videoTitle);
-      console.log("Video ID:", videoId);
-      console.log("Video Link:", videoLink);
-      console.log("Thumbnail URL:", video.snippet.thumbnails.default.url);
-      console.log("----------------------------------------");
+        console.log("Title:", videoTitle);
+        console.log("Video ID:", videoId);
+        console.log("Video Link:", videoLink);
+        console.log("Thumbnail URL:", video.snippet.thumbnails.default.url);
+        console.log("----------------------------------------");
+        bot.sendPhoto(chatId, imageUrl, {
+          caption: `${videoTitle} ------ Download here ${videoLink}`,
+          parse_mode: "Markdown",
+        });
+      });
     });
-  });
-
-  bot.sendPhoto(chatId, imageUrl, {
-    caption: `${videoTitle}------ Download mp3 here ${videoLink}`,
-    parse_mode: "Markdown",
-  });
+  }
 });
